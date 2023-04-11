8e3a1f02f8ad478f185ac1444964bda8c216955d
Gleb Golovnev <glebgolovnev2004@gmail.com>
2023-04-11 19:36:03 +0300

    Change constraints in exchange_rates table

--- src/main/resources/schemas.sql
+++ src/main/resources/schemas.sql
@@ -1,8 +1,14 @@
 CREATE TABLE IF NOT EXISTS currencies
 (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    code VARCHAR(3) UNIQUE NOT NULL,
-    full_name NVARCHAR(45) UNIQUE NOT NULL,
+    id INTEGER
+        CONSTRAINT PK_currencies_id
+            PRIMARY KEY AUTOINCREMENT,
+    code VARCHAR(3)
+        CONSTRAINT UQ_currencies_code
+            UNIQUE NOT NULL,
+    full_name NVARCHAR(45)
+        CONSTRAINT UQ_currencies_full_name
+            UNIQUE NOT NULL,
     sign VARCHAR(2) NOT NULL
 );
 
@@ -15,15 +21,22 @@ ON currencies(code);
 
 CREATE TABLE IF NOT EXISTS exchange_rates
 (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    base_currency_id INTEGER NOT NULL REFERENCES currencies(id) ON DELETE CASCADE,
-    target_currency_id INTEGER NOT NULL REFERENCES currencies(id) ON DELETE CASCADE,
+    id INTEGER
+        CONSTRAINT PK_exchange_rates_id
+            PRIMARY KEY AUTOINCREMENT,
+    base_currency_id INTEGER NOT NULL
+        CONSTRAINT FK_exchange_rates_base_currency_id
+            REFERENCES currencies(id) ON DELETE CASCADE,
+    target_currency_id INTEGER NOT NULL
+        CONSTRAINT FK_exchange_rates_target_currency_id
+            REFERENCES currencies(id) ON DELETE CASCADE,
     rate DECIMAL(6) NOT NULL,
-    CONSTRAINT unique_currencies CHECK (base_currency_id != target_currency_id)
+
+    CONSTRAINT CK_not_equals_currencies CHECK (base_currency_id != target_currency_id)
 );
 
 CREATE INDEX IF NOT EXISTS idx_id
 ON exchange_rates(id);
 
-CREATE INDEX IF NOT EXISTS idx_currencies
+CREATE UNIQUE INDEX IF NOT EXISTS idx_unique_currencies
 ON exchange_rates(base_currency_id, target_currency_id);
