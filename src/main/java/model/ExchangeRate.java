699992365df2834c3a55e02b024c3e37a46b4984
Gleb Golovnev <glebgolovnev2004@gmail.com>
2023-04-11 15:37:42 +0300

    Add BigDecimal rate

--- src/main/java/model/ExchangeRate.java
+++ src/main/java/model/ExchangeRate.java
@@ -2,9 +2,12 @@ package model;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class ExchangeRate {
     private int id;
     private Currency baseCurrency;
     private Currency targetCurrency;
+    private BigDecimal rate;
 }
