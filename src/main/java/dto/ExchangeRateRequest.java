516b7df2eb6d53e046c4ea46f485a1cb797f49e4
Gleb Golovnev <glebgolovnev2004@gmail.com>
2023-04-26 11:00:07 +0300

    Add AllArgsConstructor

--- src/main/java/dto/ExchangeRateRequest.java
+++ src/main/java/dto/ExchangeRateRequest.java
@@ -1,10 +1,12 @@
 package dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@AllArgsConstructor
 public class ExchangeRateRequest {
     private String baseCurrencyCode;
     private String targetCurrencyCode;
